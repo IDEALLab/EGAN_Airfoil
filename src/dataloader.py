5d6e329700075e7aa5665c79688b05f1bc37b0e0
Qiuyi <qchen24@ncsu.edu>
2020-09-08 01:41:11 -0400

    create dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,6 +1,47 @@
 import numpy as np
+import torch
+from torch.utils.data import DataLoader, Dataset
+from utils.interpolation import interpolate
 
-data_fname = './data/airfoil_interp.npy'
-X = np.load(data_fname)
+class UIUCAirfoilDataset(Dataset):
+    r"""UIUC Airfoil Dataset. 
 
-print(X.shape)
+    Args:
+        data_fname: Directory to the UIUC airfoil .npy file.
+        N: Number of data points.
+        k: Degree of spline.
+        D: Shifting constant. The higher the more uniform the data points are.
+    Shape:
+        Output: `(N, D, DP)` where D is the dimension of each point and DP is the number of data points.
+    """
+
+    def __init__(self, data_fname, N=192, k=3, D=20):
+        super(UIUCAirfoilDataset).__init__()
+        self.airfoils = np.load(data_fname).transpose((0, 2, 1))
+        if (N, k, D) == (192, 3, 20):
+            self.N = N; self.k = k; self.D = D
+        else:
+            self.refresh(N, k, D)
+
+    def refresh(self, N, k, D):
+        self.N = N; self.k = k; self.D = D
+        self.airfoils = np.array([interpolate(airfoil, N, k, D) for airfoil in self.airfoils])
+    
+    def __getitem__(self, index):
+        return self.airfoils[index]
+    
+    def __len__(self):
+        return len(self.airfoils)
+    
+    def __str__(self):
+        return '<UIUC Airfoil Dataset (size={}, resolution={}, spline degree={}, uniformity={})>'.format(
+            self.__len__(), self.N, self.k, self.D
+        )
+
+
+if __name__ == '__main__':
+    data_fname = './data/airfoil_interp.npy'
+    dataset = UIUCAirfoilDataset(data_fname)
+    print(dataset)
+    dataloader = DataLoader(dataset, 128)
+    print(list(dataloader)[0].shape)
