c83a1b8ee8914cd7af95d677fa5bd5f1e168cf5d
Qiuyi <qchen24@ncsu.edu>
2020-09-16 23:40:08 -0400

    likelihood evaluator

--- egan_airfoil/model/gans.py
+++ egan_airfoil/model/gans.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 from cmpnts import BezierGenerator, OTInfoDiscriminator1D
 from utils import strong_convex_func, cross_distance, first_element
 
+_eps = 1e-7
+
 class GAN:
     def __init__(
         self, generator: nn.Module, discriminator: nn.Module, 
@@ -32,14 +34,14 @@ class GAN:
     
     def _batch_hook(self, i, batch, **kwargs): pass
     
-    def _batch_report(self, i, batch, **kwargs): pass
+    def _batch_report(self, i, batch, noise_gen, **kwargs): pass
 
     def _epoch_hook(self, epoch, epochs, **kwargs): pass
 
-    def _epoch_report(self, epoch, epochs, batch, report_interval, **kwargs):
+    def _epoch_report(self, epoch, epochs, batch, noise_gen, report_interval, **kwargs):
         if epoch % report_interval == 0:
             print('[Epoch {}/{}] D loss: {:d}, G loss: {:d}'.format(
-                epoch, epochs, loss_D(batch), loss_G(batch)))
+                epoch, epochs, loss_D(batch, noise_gen), loss_G(batch, noise_gen)))
 
     def save(self, **kwargs):
         torch.save({
@@ -58,8 +60,8 @@ class GAN:
         self.optimizer_G.load_state_dict(ckp['optimizer_G'])
         self.discriminator.eval(); self.generator.eval()
     
-    def generate(self, input):
-        return first_element(self.generator(input))
+    def generate(self, noise):
+        return first_element(self.generator(noise))
 
     def train(
         self, dataloader, noise_gen, epochs, num_iter_D=5, num_iter_G=1, report_interval=5,
@@ -79,13 +81,17 @@ class GAN:
                     self.optimizer_G.zero_grad()
                     self.loss_G(batch, noise_gen, **kwargs).backward()
                     self.optimizer_G.step()
-                self._batch_report(i, batch, **kwargs)
-            self._epoch_report(epoch, epochs, batch, report_interval, **kwargs)
+                self._batch_report(i, batch, noise_gen, **kwargs)
+            self._epoch_report(epoch, epochs, batch, noise_gen, report_interval, **kwargs)
 
             if save_iter_list and epoch in save_iter_list:
                 self.save(epoch=epoch, noise=noise_gen)
 
 class EGAN(GAN):
+    def __init__(self, generator, discriminator, lamb, name, save_dir, checkpoint=None):
+        super().__init__(generator, discriminator, name, save_dir, checkpoint=checkpoint)
+        self.lamb = lamb
+
     def loss_D(self, batch, noise_gen, **kwargs):
         return -self.loss_G(batch, noise_gen)
     
@@ -100,9 +106,39 @@ class EGAN(GAN):
 
         emd = torch.mean(d_r) - torch.mean(d_f) # E[D1] - E[D2]
         v = torch.squeeze(d_r.unsqueeze(0) - d_f.unsqueeze(1)) \
-            - cross_distance(real, fake) # v(y,^y) grid across different y and ^y
-        smooth = strong_convex_func(v, lamb=self.lamb)
+            - cross_distance(real, fake) # v(y,^y) grid across different y and ^y. Should add one argument to choose distance functions.
+        smooth = strong_convex_func(v, lamb=self.lamb).mean()
         return emd - smooth
+    
+    def _estimate_probs(self, noise, fake, test):
+        """
+        Estimate P(x|y=y_test) in 4.3
+        """
+        # unnormalized density
+        p_x = torch.exp(torch.diag(-0.5 * np.dot(noise, noise.T))) # [batch, 1] Gaussian P(x) unnormalized. 
+        # Probability of x of every entry in a batch. Should be modified to be compatible with infogan.
+
+        d_r = first_element(self.discriminator(test))[:, 0]
+        d_f = first_element(self.discriminator(fake))[:, 1]
+        v_star = (torch.squeeze(d_r.unsqueeze(0) - d_f.unsqueeze(1), dim=-1) \
+            - cross_distance(test, fake)) / self.lamb # [batch, n_test] term inside exp() of 4.3. 
+        exp_v = torch.exp(v_star - v_star.max(dim=0)) # [batch, n_test] avoid numerical instability.
+        prob = p_x * exp_v # [batch, n_test] unnormalized 4.3
+        return prob / prob.sum(dim=0) # [batch, n_test] normalize over x to obtain 4.3 P(x|y)
+
+    def compute_LL(self, test, noise_gen):
+        noise = noise_gen(); fake = self.generate(noise)
+        prob = self._estimate_probs(noise, fake, test) # [batch, n_test] P(x|y)
+        
+        dist = cross_distance(test, fake) # [batch, n_test]
+        e_dist = torch.sum(dist * prob, dim=0) # [n_test] expectation of dist
+        ent = torch.sum(-prob * torch.log(prob + _eps), dim=0) # [n_test] entropy
+
+        e_norm = torch.sum(-0.5 * (noise**2).sum(dim=1, keepdim=True) * prob, dim=0) # [n_test] expectation of norm. Should this be L2 for all dimension?
+        # computing likelihood of the latent variable? Should be modified to accommodate uniformly distributed latent variables
+
+        lower_bound = -1 / self.lamb * (e_dist - self.lamb * ent) + e_norm # [n_test] log likelihood lower bound 2.7  
+        return lower_bound 
     
     def _epoch_hook(self, epoch, epochs, **kwargs):
         if epoch == 0:
@@ -113,25 +149,28 @@ class EGAN(GAN):
 
 class InfoEGAN(EGAN):
     def loss_D(self, batch, noise_gen, **kwargs):
-        noise = noise_gen(); latent_dim = noise_gen.sizes[0]
+        noise = noise_gen(); latent_code = noise[:, :noise_gen.sizes[0]]
         fake = first_element(self.generator(noise))
         dual_loss = self.dual_loss(batch, fake)
-        info_loss = F.mse_loss(self.discriminator(fake)[1], noise[:, :latent_dim])
+        info_loss = self.info_loss(fake, latent_code)
         return -dual_loss + info_loss
     
     def loss_G(self, batch, noise_gen, **kwargs):
-        noise = noise_gen(); latent_dim = noise_gen.sizes[0]
+        noise = noise_gen(); latent_code = noise[:, :noise_gen.sizes[0]]
         fake = first_element(self.generator(noise))
         dual_loss = self.dual_loss(batch, fake)
-        info_loss = F.mse_loss(self.discriminator(fake)[1], noise[:, :latent_dim])
+        info_loss = self.info_loss(fake, latent_code)
         return dual_loss + info_loss
+    
+    def info_loss(self, fake, latent_code):
+        return F.mse_loss(self.discriminator(fake)[1], latent_code)
 
 class BezierEGAN(InfoEGAN):
     def loss_G(self, batch, noise_gen, **kwargs):
-        noise = noise_gen(); latent_dim = noise_gen.sizes[0]
+        noise = noise_gen(); latent_code = noise[:, :noise_gen.sizes[0]]
         fake, cp, w, pv, intvls = self.generator(noise)
         dual_loss = self.dual_loss(batch, fake)
-        info_loss = F.mse_loss(self.discriminator(fake)[1], noise[:, :latent_dim])
+        info_loss = self.info_loss(fake, latent_code)
         reg_loss = self.regularizer(cp, w, pv, intvls)
         return dual_loss + info_loss + 10 * reg_loss
     
--- egan_airfoil/model/utils.py
+++ egan_airfoil/model/utils.py
@@ -3,31 +3,25 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 def cross_distance(x, y, diag=False, useCosineDist=False):
+    x = x.reshape([len(x), -1]); y = y.reshape([len(y), -1])
     if useCosineDist: # should call similarity function
-        return F.cosine_similarity(x.unsqueeze(0), y.unsqueeze(1), dim=2)
+        return F.cosine_similarity(x.unsqueeze(0), y.unsqueeze(1), dim=-1)
     else:
-        return torch.sum(torch.abs(x.unsqueeze(0) - y.unsqueeze(1)), 2) # shape [batch, batch]
+        return torch.sum(torch.abs(x.unsqueeze(0) - y.unsqueeze(1)), dim=-1) # shape [batch, batch]
 
-def strong_convex_func(x, lamb, mean=True, useHingedL2=False):
+def strong_convex_func(x, lamb, useHingedL2=False):
     if useHingedL2:
         func = (torch.maximum(x, 0) ** 2) / lamb / 2.
     else:
         func = torch.exp(x / lamb) / torch.exp(1.) * lamb
-    if mean:
-        return torch.mean(func)
-    else:
-        return func
+    return func
 
-def strong_convex_func_normalized(x, lamb, mean=False, useHingedL2=False): # without lamb
+def strong_convex_func_normalized(x, lamb, useHingedL2=False): # without lamb
     if useHingedL2:
         func = (torch.maximum(x, 0) ** 2) / 2.
     else:
         func = torch.exp(x / lamb) / torch.exp(1.)
-
-    if mean:
-        return torch.mean(func)
-    else:
-        return func
+    return func
 
 def sum_probs_func(x, lamb):
     return torch.mean(torch.maximum(x, 0.0)) / lamb
